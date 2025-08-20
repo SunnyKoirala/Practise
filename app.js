26b2df136e070e53f54fad3cb1cb0e9bfc98355e
Sunny Koirala <sunnykoirala777@gmail.com>
2025-08-20 09:00:45 +0545

    made some changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-
+const Listing=require("./models/listing");
 
 app.get("/",(req,res)=>{
     res.send("Hi, I am root");
@@ -16,6 +16,19 @@ main().then(()=>{
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/practise");
 }
+
+app.get("/testListing",async(req,res)=>{
+    let listing=new Listing({
+        title:"My Home",
+        description:"Beautiful and Peaceful Place",
+        price:1500,
+        location:"Kathmandu",
+        country:"Nepal"
+    });
+    await listing.save();
+    console.log("Data saved into DB");
+    res.send("Data saved successfully");
+})
 app.listen(8080,()=>{
     console.log("Server is Listening to port no 8080");
 });
