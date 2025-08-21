44ed78ef40357b22ac40e8e8de0043c3a40d0d6d
Sunny Koirala <sunnykoirala777@gmail.com>
2025-08-21 16:03:47 +0545

    made some changes

---
+++ app.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+main().then(()=>{
+    console.log("successfully connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+async function main()
+{
+    await mongoose.connect("mongodb://127.0.0.1:27017/practise");
+}
+app.listen(8080,()=>{
+    console.log("Server is listening to port no 8080");
+});
+
+
+app.get("/",(req,res)=>{
+    res.send("Hi, I am root");
+});
