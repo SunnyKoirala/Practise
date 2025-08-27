43308c60c55b0938fd98cd6253f80895dd9f335d
Sunny Koirala <sunnykoirala777@gmail.com>
2025-08-27 16:49:28 +0545

    Inital commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Listing=require("./models/listing");
+main().then(()=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+async function main()
+{
+    mongoose.connect("mongodb://127.0.0.1:27017/practise");
+}
+app.get("/",(req,res)=>{
+    res.send("This is root");
+});
+
+app.get("/testListing",async(req,res)=>{
+    let newListing=new Listing({
+        title:"My home",
+        description:"Peaceful Area",
+        price:2500,
+        location:"Kathmandu",
+        country:"Nepal"
+    })
+    await newListing.save();
+    console.log("data has been saved successfully");
+    res.send("data has been saved successfully");
+});
+
+
+
+app.listen(8080,()=>{
+    console.log("Server is listeneing to port 8080");
+});
+
